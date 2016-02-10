159ac6ba95d6d170a7841607a8efd9c9e9692a50
Luke Picciau <luke.b.picciau@gmail.com>
2016-02-10 20:33:56 +1030

    Commented code

--- subarchive.py
+++ subarchive.py
@@ -4,9 +4,13 @@ import redis
 
 import config
 
-r = praw.Reddit(user_agent=config.USER_AGENT)
+r = praw.Reddit(user_agent=config.USER_AGENT) #Make connection to reddit
 
 class archiver():
+    '''
+    *   Redis is used to find the IPFS hash for a post using its post ID
+    *   Connection to redis made here
+    '''
     def __init__(self, *args, **kwargs):
         self.r = redis.StrictRedis(host=config.REDIS_HOST,
                                port=config.REDIS_PORT,
@@ -17,7 +21,8 @@ class archiver():
 
 
 while True:
+    #Try to get the newest posts. There should be a better way to do this
     submissions = r.get_subreddit(config.SUBREDDIT).get_new(limit=config.LIMIT)
     for submission in submissions:
-        print str(submission.id)
+        print str(submission.id) #Just used for debuging now
     time.sleep(10)
